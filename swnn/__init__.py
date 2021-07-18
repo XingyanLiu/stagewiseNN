19ce39d0f160715a6fce26a9e6ec0c97bed1079b
liuxingyan <liuxingyan@meituan.com>
2021-07-18 18:14:19 +0800

    get_data

---
+++ swnn/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+"""
+@CreateDate: 2020/07/18
+@Author: Xingyan Liu
+@File: __init__.py
+@Project: stagewiseNN
+"""
+
+from .utils import *
+from .graph import *
+from .graph2tree import *
+
+
+
--- test_main.py
+++ test_main.py
@@ -12,10 +12,21 @@ from typing import Sequence, Mapping, Optional, Union, Callable
 import logging
 import pandas as pd
 import numpy as np
+import scanpy as sc
+
+ROOT = os.path.dirname(__file__)
+sys.path.append(ROOT)
+
+
+def get_adata():
+    path = f'{ROOT}/sample_data/merged_B-L0-0.2.h5ad'
+    adata = sc.read_h5ad(path)
+    return adata
 
 
 def __test__():
-    pass
+    adata = get_adata()
+    logging.info(adata)
 
 
 if __name__ == '__main__':
